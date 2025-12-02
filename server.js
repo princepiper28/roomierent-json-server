dd305d753049fcf64395ffe7f865c1724f21da57
prince joseph udoewah <princepiper28@gmail.com>
2025-12-02 23:25:54 +0100

    Initial json-server backend

---
+++ server.js
@@ -0,0 +1,24 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+// Enable CORS
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  next();
+});
+
+// Start server
+server.use(router);
+server.listen(5000, () => {
+  console.log("RoomieRent JSON Server is running on port 5000");
+});
